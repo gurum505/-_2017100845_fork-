d0b916c8aac6b6931760a3974d4e4716366e3c0b
seho jeong <gurum505@khu.ac.kr>
2020-10-16 10:50:42 +0900

    1 to 8

---
+++ multiplication.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 8; //n=8--> n=9
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            std::cout << i << " * " << j << "= " << i * j << std::endl;
+        }
+        std::cout << "----------" << std::endl;
+    }
+    std::cout << "Hello World!\n";
+}
