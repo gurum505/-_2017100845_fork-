f5c31139eeb6f931d716b177c15cc2e19d2601b8
minsik cho <olivetree421@naver.com>
2020-10-16 11:08:59 +0900

    9단까지 수정완료

--- multiplication.cpp
+++ multiplication.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int n = 8; //n=8--> n=9
+    int n = 9; //n=8--> n=9
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             std::cout << i << " * " << j << "= " << i * j << std::endl;
